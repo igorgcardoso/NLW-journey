8df790d18463af38543ba366348d1bdd97497fa9
Igor Cardoso <igorgcardoso@protonmail.com>
2024-07-18 10:59:10 -0300

    lecture 2: nodejs

---
+++ node/src/routes/create-activity.ts
@@ -0,0 +1,55 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { dayjs } from "../lib/dayjs";
+import { prisma } from "../lib/prisma";
+
+export async function createActivity(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/trips/:tripId/activities",
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string().uuid(),
+        }),
+        body: z.object({
+          title: z.string().min(4),
+          occursAt: z.coerce.date(),
+        }),
+      },
+    },
+    async (request) => {
+      const { tripId } = request.params;
+      const { title, occursAt } = request.body;
+
+      const trip = await prisma.trip.findUnique({
+        where: {
+          id: tripId,
+        },
+      });
+
+      if (!trip) {
+        throw new Error("Trip not found");
+      }
+
+      if (
+        dayjs(occursAt).isBefore(trip.starts_at) ||
+        dayjs(occursAt).isAfter(trip.ends_at)
+      ) {
+        throw new Error("Invalid activity date");
+      }
+
+      const activity = await prisma.activity.create({
+        data: {
+          title,
+          occurs_at: occursAt,
+          trip_id: trip.id,
+        },
+      });
+
+      return {
+        activityId: activity.id,
+      };
+    },
+  );
+}
--- node/src/server.ts
+++ node/src/server.ts
@@ -1,11 +1,16 @@
+import cors from "@fastify/cors";
 import fastify from "fastify";
-import { createTrip } from "./routes/create-trip";
 import {
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
+import { confirmParticipant } from "./routes/confirm-participant";
 import { confirmTrip } from "./routes/confirm-trip";
-import cors from "@fastify/cors";
+import { createActivity } from "./routes/create-activity";
+import { createLink } from "./routes/create-link";
+import { createTrip } from "./routes/create-trip";
+import { getActivities } from "./routes/get-activities";
+import { getLinks } from "./routes/get-links";
 
 const app = fastify();
 
@@ -18,6 +23,11 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
 app.register(confirmTrip);
+app.register(confirmParticipant);
+app.register(createActivity);
+app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running");
